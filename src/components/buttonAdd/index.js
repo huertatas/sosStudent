4764e0160d70fe36df67d12da27a538ca59052ae
huertatas <max.54.1990@gmail.com>
2022-04-07 13:00:08 +0200

    buton finish

--- src/components/buttonAdd/index.js
+++ src/components/buttonAdd/index.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import styled from 'styled-components'
 import Ionicons from 'react-native-vector-icons/Ionicons'
-Ionicons.loadFont().then();
+Ionicons.loadFont().then()
 import { colors } from '../../helpers/colors'
 
 export default function ButtonAdd({ textButton }) {
-  return <ButtonAddStyled></ButtonAddStyled>
+  return (
+    <ButtonAddStyled>
+      <Ionicons name={'add-outline'} size={30} color='white' />
+    </ButtonAddStyled>
+  )
 }
 
 const ButtonAddStyled = styled.TouchableOpacity`
--- src/components/buttonValidity/index.js
+++ src/components/buttonValidity/index.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import styled from 'styled-components'
 import Ionicons from 'react-native-vector-icons/Ionicons'
-Ionicons.loadFont().then();
+Ionicons.loadFont().then()
 import { colors } from '../../helpers/colors'
 
 export default function ButtonValidity({ textButton }) {
   return (
     <ButtonValidityStyled>
-      <Ionicons name={'home-outline'} size={30} />
+      <Ionicons name={'checkmark-outline'} size={30} color='white' />
     </ButtonValidityStyled>
   )
 }
@@ -20,7 +20,6 @@ const ButtonValidityStyled = styled.TouchableOpacity`
   display: flex;
   justify-content: center;
   align-items: center;
-  margin: auto;
   margin-top: 20px;
   margin-bottom: 20px;
   padding: 0px;
