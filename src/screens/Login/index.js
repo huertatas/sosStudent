8d3f24f0ba33c69c8272a0a7c3468287c1f24f00
ismailaissami <aissami.ismail@gmail.com>
2022-04-08 12:02:42 +0200

    login redux

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -1,27 +1,44 @@
 import React, { useEffect, useState } from 'react'
 import { showMessage } from 'react-native-flash-message'
 import styled from 'styled-components'
-import Inputs from '../../components/TextInput'
+import Input from '../../components/TextInput'
 import Image from '../../components/Image'
 
+import { useDispatch, useSelector } from 'react-redux'
+import { login } from '../../actions/login'
+
 const Login = ({ navigation }) => {
+
+  const token = useSelector(state=> state.token)
+  const dispatch = useDispatch()
+
+  const [email, setEmail] =useState('')
+  const [password, setPassword] =useState('') 
+
+  useEffect(()=> {
+    console.log(`🚧👷 Debug  : token = ${token}`)
+  }, [token])
+
+  useEffect(()=> {
+    console.log(`♿️♿️ Debug  : email = ${email},  password= ${password}`)
+  }, [email, password])
+
   const handleSubmit = () => {
-    navigation.navigate('ProfessorWaitingRoom')
+    dispatch(login({email, password}))
+    //navigation.navigate('ProfessorWaitingRoom')
   }
 
   return (
     <Container>
       <Image uri='https://edtechawesomeness.files.wordpress.com/2017/09/share-to-classroom-e1504657407631.png' />
-      <Inputs placeholder='Identifiant' />
-      <Inputs placeholder='Mot de passe' />
-      <Submit title='Se connecter' onPress={handleSubmit} />
+      <Input placeholder='Email...' value={email} onChangeValue={setEmail}/>
+      <Input placeholder='Password...' value={password} onChangeValue={setPassword}/>
+      <Submit title='Login' onPress={handleSubmit} />
     </Container>
   )
 }
 
 const Container = styled.View``
-
-const Input = styled.TextInput``
 
 const Submit = styled.Button``
 
