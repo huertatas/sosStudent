ccb30d29da60689c3444d53a70838c1d2d73ded5
huertatas <max.54.1990@gmail.com>
2022-04-08 12:25:07 +0200

    login clean

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -8,31 +8,34 @@ import { useDispatch, useSelector } from 'react-redux'
 import { login } from '../../actions/login'
 
 const Login = ({ navigation }) => {
-
-  const token = useSelector(state=> state.token)
+  const token = useSelector(state => state.token)
   const dispatch = useDispatch()
 
-  const [email, setEmail] =useState('')
-  const [password, setPassword] =useState('') 
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
 
-  useEffect(()=> {
+  useEffect(() => {
     console.log(`🚧👷 Debug  : token = ${token}`)
   }, [token])
 
-  useEffect(()=> {
+  useEffect(() => {
     console.log(`♿️♿️ Debug  : email = ${email},  password= ${password}`)
   }, [email, password])
 
   const handleSubmit = () => {
-    dispatch(login({email, password}))
-    //navigation.navigate('ProfessorWaitingRoom')
+    // dispatch(login({email, password}))
+    navigation.navigate('ProfessorClassRoom')
   }
 
   return (
     <Container>
       <Image uri='https://edtechawesomeness.files.wordpress.com/2017/09/share-to-classroom-e1504657407631.png' />
-      <Input placeholder='Email...' value={email} onChangeValue={setEmail}/>
-      <Input placeholder='Password...' value={password} onChangeValue={setPassword}/>
+      <Input placeholder='Email...' value={email} onChangeValue={setEmail} />
+      <Input
+        placeholder='Password...'
+        value={password}
+        onChangeValue={setPassword}
+      />
       <Submit title='Login' onPress={handleSubmit} />
     </Container>
   )
