d1d8db078086e68c3cc22fea905a00b68d57f81a
huertatas <max.54.1990@gmail.com>
2022-04-10 21:16:54 +0200

    counter redux delete

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,9 @@
 import { combineReducers } from 'redux'
-import counter from './counter'
 import rooms from './room'
 import notifs from './notif'
 import login from './login'
 
 export default combineReducers({
-  counter,
   rooms,
   notifs,
   login
