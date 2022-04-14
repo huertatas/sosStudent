97cf950aa362bead3584ab8675d87f27653e9d78
huertatas <max.54.1990@gmail.com>
2022-04-14 21:20:42 +0200

    requesr notifee ios

--- App.js
+++ App.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Routes from './src/config/routes'
 import { View } from 'react-native'
 import styled from 'styled-components'
 import FlashMessage from 'react-native-flash-message'
 import { Provider } from 'react-redux'
 import { store } from './src/config/store'
+
+import notifee, { AuthorizationStatus } from '@notifee/react-native'
+
+async function requestUserPermission() {
+  const settings = await notifee.requestPermission()
+
+  if (settings.authorizationStatus) {
+    console.log('User has notification permissions enabled')
+  } else {
+    if (settings.authorizationStatus >= AuthorizationStatus.AUTHORIZED) {
+      console.log('Permission settings:', settings)
+    } else {
+      console.log('User declined permissions')
+    }
+  }
+}
 
 const App = () => {
   return (
--- index.js
+++ index.js
@@ -6,16 +6,4 @@ import { AppRegistry } from 'react-native'
 import App from './App'
 import { name as appName } from './app.json'
 
-import notifee, { AuthorizationStatus } from '@notifee/react-native'
-
-async function requestUserPermission() {
-  const settings = await notifee.requestPermission()
-
-  if (settings.authorizationStatus >= AuthorizationStatus.AUTHORIZED) {
-    console.log('Permission settings:', settings)
-  } else {
-    console.log('User declined permissions')
-  }
-}
-
 AppRegistry.registerComponent(appName, () => App)
