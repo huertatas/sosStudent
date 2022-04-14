e3a3807327adcc1a77363a3689454e5e540d3d20
huertatas <max.54.1990@gmail.com>
2022-04-14 21:15:43 +0200

    request notifee

--- index.js
+++ index.js
@@ -6,4 +6,16 @@ import { AppRegistry } from 'react-native'
 import App from './App'
 import { name as appName } from './app.json'
 
+import notifee, { AuthorizationStatus } from '@notifee/react-native'
+
+async function requestUserPermission() {
+  const settings = await notifee.requestPermission()
+
+  if (settings.authorizationStatus >= AuthorizationStatus.AUTHORIZED) {
+    console.log('Permission settings:', settings)
+  } else {
+    console.log('User declined permissions')
+  }
+}
+
 AppRegistry.registerComponent(appName, () => App)
