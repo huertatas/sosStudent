e35ef755e7436efb396c5dcc45891b5669be3df9
huertatas <max.54.1990@gmail.com>
2022-04-14 19:32:48 +0200

    rectif index

--- index.js
+++ index.js
@@ -5,10 +5,13 @@
 import { AppRegistry } from 'react-native'
 import App from './App'
 import { name as appName } from './app.json'
+
 import notifee, { EventType } from '@notifee/react-native'
 
 notifee.onBackgroundEvent(async ({ type, detail }) => {
   const { notification, pressAction } = detail
+
+  console.log('passe ici')
 
   const channelId = await notifee.createChannel({
     id: 'default',
